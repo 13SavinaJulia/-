bfa4d567f9aa8b2aed34ca0469eb9c6292afa281
Юлия <72439922+13SavinaJulia@users.noreply.github.com>
2020-10-10 18:13:21 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class SHEIN
+    {
+        string Name;
+        double number;
+        string purchases;
+
+        public SHEIN(string name, double number, string purchases)
+        {
+            Name = name;
+            this.number = number;
+            this.purchases = purchases;
+        }
+        public string Sum(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+        private string ReturnNameSHEIN()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class SHEIN
+    {
+        static void Main(string[] args)
+        {
+            SHEIN shop = new SHEIN("Ксения", "Покупки");
+            Console.WriteLine(shop.ReturnNameSHEIN());
+            shop.SetName("Вера");
+            Console.WriteLine(shop.ReturnNameSHEIN());
+            Console.ReadLine();
+        }
+    }
+}
